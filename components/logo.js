61d162c454839a7b21bfcb4c284dc99f2b2424ce
Manuel Duarte <carlosd99m@gmail.com>
2021-10-31 16:03:12 -0600

    ⚡ feat(ui): navbar update

--- components/logo.js
+++ components/logo.js
@@ -35,7 +35,7 @@ const Logo = () => {
             fontWeight="bold"
             ml={3}
           >
-            Takuya Matsuyama
+            Manuel Duarte
           </Text>
         </LogoBox>
       </a>
