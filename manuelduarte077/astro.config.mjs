7d68caf2c858b7c281ad4bb81078ecc59cbe73a5
Manuel Duarte  <carlosd99m@gmail.com>
2023-03-09 00:44:12 -0600

    chore: add dependencies

--- manuelduarte077/astro.config.mjs
+++ manuelduarte077/astro.config.mjs
@@ -1,10 +1,25 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	site: "https://gndx.dev",
+	integrations: [
+		mdx({}),
+		react(),
+		tailwind({
+			config: { applyBaseStyles: false },
+		}),
+		image(),
+		sitemap(),
+	],
 });
